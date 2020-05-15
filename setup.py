eed9e10f8139bb155b2f1a2a69e19001b05bd29b
DRodriguez615 <Drjrodriguez615@gmail.com>
2020-05-15 16:11:27 -0400

    Function Test

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import find_packages, setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
 setup(
     name="lambdata-pt5",
     version="1.0",
@@ -11,10 +12,9 @@ setup(
     author_email="drjrodriguez615@gmail.com",
     description="For example purposes",
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    # required if using a md file for long desc
+    long_description_content_type="text/markdown",# required if using a md file for long desc
     # license="MIT",
     url="https://github.com/DRodriguez615/lambdata-pt5",
     # keywords="",
-    packages=find_packages()  # ["my_lambdata"]
+    packages= ["my_lambdata"] #find_packages()  # ["my_lambdata"]
 )
